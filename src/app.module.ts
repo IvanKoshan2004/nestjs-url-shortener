dac595abd3cb38b77633abddf255da651a76d365
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-07-09 14:22:37 +0300

    added mongodb and mongoose to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { UrlshortenerModule } from './urlshortener/urlshortener.module';
 import { ShortenerModule } from './shortener/shortener.module';
-import { RedirectController } from './redirect/redirect.controller';
-import { RedirectService } from './redirect/redirect.service';
-import { RedirectModule } from './redirect/redirect.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { username, password } from './mongoose.config.json';
 @Module({
-    imports: [UserModule, UrlshortenerModule, ShortenerModule, RedirectModule],
-    controllers: [AppController, RedirectController],
-    providers: [AppService, RedirectService],
+    imports: [
+        UserModule,
+        ShortenerModule,
+        MongooseModule.forRoot(
+            `mongodb+srv://${username}:${password}@maincluster.vettm3l.mongodb.net/`,
+        ),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
