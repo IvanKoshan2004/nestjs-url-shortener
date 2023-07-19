bc5bec357a5ebb05e1b4613146af7ab1f4f518a9
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-07-19 14:46:43 +0300

    added functionality to the controller try catch wrapper function

--- src/lib/controller-try-catch-wrapper.ts
+++ src/lib/controller-try-catch-wrapper.ts
@@ -1,18 +1,36 @@
+import { Response } from 'express';
 export async function controllerTryCatchWrapper(
-    callback: () => any,
+    callback: (messages?: { errorMessage?; successMessage? }) => any,
     options: {
+        res?: Response;
         successMessage?: string;
         errorMessage?: string;
-        showError: boolean;
-    } = {
+        showError?: boolean;
+    } = {},
+): Promise<{ message: string; status: string; data?: any; error?: string }> {
+    const modifiedMessages: { errorMessage?; successMessage? } = {};
+    const defaultOptions = {
         successMessage: 'Action successful',
         errorMessage: 'Error occured',
         showError: true,
-    },
-): Promise<{ message: string; status: string; data?: any; error?: string }> {
+    };
+    options = Object.assign({}, options, defaultOptions);
+    let response;
     try {
-        return { message: options.successMessage, status: 'ok', data: await callback() };
+        const data = await callback(modifiedMessages);
+        response = { message: options.successMessage, status: 'ok', data };
+        if (modifiedMessages.successMessage) {
+            response.message = modifiedMessages.successMessage;
+        }
     } catch (error) {
-        return { message: options.errorMessage, status: 'error', error: options.showError ? error : '' };
+        response = { message: options.errorMessage, status: 'error', error: options.showError ? error : '' };
+        if (modifiedMessages.errorMessage) {
+            response.message = modifiedMessages.errorMessage;
+        }
+    } finally {
+        if (options.res) {
+            options.res.json(response);
+        }
+        return response;
     }
 }
